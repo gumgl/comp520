cbbd6358e2e8fab71eadeb3f46ab7b36759c5119
William Bain <bain.william.a@gmail.com>
2015-02-13 13:53:23 -0500

    Misc whitespace fixes

--- src/CustomASTPrinter.java
+++ src/CustomASTPrinter.java
@@ -2,27 +2,27 @@
 import golite.node.* ;
 import golite.analysis.* ;
 
-import java.lang.System; 
-  
-public class CustomASTPrinter extends DepthFirstAdapter { 
+import java.lang.System;
+
+public class CustomASTPrinter extends DepthFirstAdapter {
 	StringBuilder indent = new StringBuilder("");
-	
+
 	/*public ASTPrinter() {
 		super()
 	}*/
-	
+
 	public void defaultIn(@SuppressWarnings("unused") Node node)
 	{
 	    // Do nothing
 		System.out.println(indent + node.getClass().getSimpleName() + ": " + node.toString());
 		indent.append("   |");
 	}
-	
+
 	public void defaultOut(@SuppressWarnings("unused") Node node)
 	{
 	    // Do nothing
 		indent.delete(0, 4);
 		//System.out.println(indent + "<" + node.getClass().getName());
 	}
-	
+
 }
--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,24 @@ import golite.parser.* ;
 import golite.lexer.* ;
 import golite.node.* ;
 
-import java.io.* ; 
+import java.io.* ;
 import java.util.Map;
-  
+
 public class Main {
 	static Map tokenMap;
 	public static void main(String[] args) {
-		if (args.length > 0) { 
-			try { 
-				/* Form our AST */ 
-				Lexer lexer = new Lexer (new PushbackReader(new FileReader(args[0]), 1024)); 
-				Parser parser = new Parser(lexer); 
+		if (args.length > 0) {
+			try {
+				/* Form our AST */
+				Lexer lexer = new Lexer (new PushbackReader(new FileReader(args[0]), 1024));
+				Parser parser = new Parser(lexer);
 				Start ast = parser.parse();
 				PrintWriter stderr = new PrintWriter(System.out, true);
-				
+
 				TokenMapper tm = new TokenMapper();
 			    ast.apply(tm);
 			    tokenMap = tm.getMap();
-			    
+
 				// Get file name
 				String path = args[0];
 				if (path.endsWith(".min")) {
@@ -29,35 +29,35 @@ public class Main {
 				}
 				// Pretty Print
 				PrintWriter filePretty = new PrintWriter(new PrintWriter(path+".pretty.min"), true);
-				PrettyPrinter pretty = new PrettyPrinter(filePretty) ; 
+				PrettyPrinter pretty = new PrettyPrinter(filePretty) ;
 				ast.apply(pretty);
 				filePretty.close();
-				
+
 				// Type Checker
 				PrintWriter fileTypeChecker = new PrintWriter(new PrintWriter(path+".symbol.txt"), true);
-				TypeChecker typechecker = new TypeChecker(fileTypeChecker, stderr) ; 
+				TypeChecker typechecker = new TypeChecker(fileTypeChecker, stderr) ;
 				ast.apply(typechecker);
 				fileTypeChecker.close();
-				
+
 				if (typechecker.success) {
 					// Generate C Code
 					PrintWriter fileCCode = new PrintWriter(path+".c");
-					CGenerator cgen = new CGenerator(fileCCode, typechecker.types, typechecker.symbols) ; 
+					CGenerator cgen = new CGenerator(fileCCode, typechecker.types, typechecker.symbols) ;
 					ast.apply(cgen);
 					fileCCode.close();
-					
+
 					System.out.println("VALID, C code generated to " + path + ".c");
 				}
-				
+
 				/*
 				// Display Typed AST in console
 				ASTPrinter typed = new ASTPrinter(typechecker.types);
 				ast.apply(typed);
-				
+
 				// Display AST in a JTree
 				ASTDisplay display = new ASTDisplay();
 				ast.apply(display);
-				
+
 				// Print Symbol Table
 				for (String id : typechecker.symbols.keySet())
 					System.out.println("<" + id + "> is " + String.valueOf(typechecker.symbols.get(id)));
