fd541858d238f1b28ccf9f811fa8ea44c9da84c7
sophiesuperduper <sophiezhang0919@gmail.com>
2015-02-14 11:39:26 -0500

    Create parser_test_invalid4.go

---
+++ programs/invalid/parser_test_invalid4.go
@@ -0,0 +1,29 @@
+//func goto package if else return
+//case defer go map struct
+
+func main() {
+  x:=28
+
+//error:even(x) used as value
+//y:=even(x) 
+
+  even(x)
+  print
+
+//error:too many arguments to return
+  return x
+}
+
+
+
+
+
+func even(x int) {
+if x%2==0 {goto skip}
+else {goto msg}
+
+msg:
+  print("odd number")
+skip:
+  print("even number")
+}
