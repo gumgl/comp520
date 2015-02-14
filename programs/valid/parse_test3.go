f0811e4eb863afe51bf7a415a756ee067f89eec3
sophiesuperduper <sophiezhang0919@gmail.com>
2015-02-14 01:26:17 -0500

    Update parse_test3.go

--- programs/valid/parse_test3.go
+++ programs/valid/parse_test3.go
@@ -1,7 +1,6 @@
 /* Package declaration, golite 2.2 */
 package parse_test3
 
-
 func main() {
 
 /* Type casts, golite 2.9.8 */
@@ -9,7 +8,6 @@ a:= 7.1415726
 b:= int64(a) // losing the decimals here
 println("should be 7 and we have...",b) //should be 7
  
-
 //initialize sum to 0
 var i,j,k,sum1,sum2,sum3 int 
 
@@ -19,28 +17,26 @@ var i,j,k,sum1,sum2,sum3 int
 
 // Infinite loop
 loop_a:
-    for {
-    sum1 = sum1+i
-    i++
-    if i <= 10 {
-        continue loop_a
-    } else {
-        break loop_a
-        }
-    }
-
+ for {
+  sum1 = sum1+i
+  i++
+  if i <= 10 {
+  continue loop_a
+  } else {
+  break loop_a
+  }
+ }
 
 // "While" loop_b
 for j < 11 {
-    sum2+=j
-    j++
+ sum2+=j
+ j++
 }
 
 // Three-part loop_c
 for k= 10; k >=0; k-- {
-    sum3+=k
+ sum3+=k
 }
 
-println ("loop a has",sum1,"\nloop b has",sum2,"\nloop c jas",sum3)
+println ("the three results should be equal and we have...","\n\nloop a has",sum1,"\nloop b has",sum2,"\nloop c jas",sum3)
 }
-
