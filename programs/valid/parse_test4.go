999f35f6126031c290c7a5dd91bbc52ed213c452
sophiesuperduper <sophiezhang0919@gmail.com>
2015-02-14 13:58:59 -0500

    Update parse_test4.go

--- programs/valid/parse_test4.go
+++ programs/valid/parse_test4.go
@@ -15,10 +15,14 @@ b:=1+71%(-13+6)*7
 c:=(1+2)/4
 d:=1+2/4
 
-s := "jibber"
-s +=(string(c) + "jaber") //string(c) does nothing
-println("the following should be 8 8 0 1 0.i 2.1i and we have...",a,b,c,d,0.i,2.1i,s)
+//+= operator
 
+s := "jibber"
+s += "jabber"
+s += " 1 "
+s +=(string(c) + "jabber") //string(c) does nothing?
+println("the following should be 8 8 0 1 and we have...",a,b,c,d,s)
+ 
 //Logical operators
 //http://golang.org/ref/spec#Operators to be added in the morning
 
