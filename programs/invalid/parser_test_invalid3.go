bb288173e7a668ca11b9ea703eaa409f1aad6285
sophiesuperduper <sophiezhang0919@gmail.com>
2015-02-14 13:06:36 -0500

    Create parser_test_invalid3.go

---
+++ programs/invalid/parser_test_invalid3.go
@@ -0,0 +1,26 @@
+package parser_test_invalid3
+
+func main(){
+ 
+//illegal backslash sequence
+//error:unknown escape sequence: '
+println("hello\'there")
+
+a:= 5
+b:= 10
+
+//no multiple returns accepted
+s,t := compare(a,b)
+println(s,t)
+
+}
+
+func compare(y int,z int)(int,int){
+if x := 3; x > y {
+	return x,y;
+} else if x > z {
+	return x,z
+} else {
+	return y,z
+}
+}
