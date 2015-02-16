db0407a63050a1b9ebdd7ec114b3be0e29c96984
William Bain <bain.william.a@gmail.com>
2015-02-16 14:01:05 -0500

    Clean up main method
    Mostly just reorganize things. Use a separate lexer instance to
    print the token output, since it consumes all the tokens.

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,78 @@
 
-//import Interpreter; 
-import golite.parser.* ; 
-import golite.lexer.* ; 
-import golite.node.* ; 
+import golite.parser.* ;
+import golite.lexer.* ;
+import golite.node.* ;
 
 import java.io.* ; 
 import java.util.Map;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-  
 public class Main {
-	static Map tokenMap;
+	static Map<Node, Token> tokenMap;
 	static String path;
 	static PrintWriter stderr;
 	public static void main(String[] args) {
-		if (args.length > 0) { 
-			try { 
-				/* Build AST */ 
-				Lexer lexer = new ConservingGoLexer(new PushbackReader(new FileReader(args[0]), 1024));
-				/*Parser parser = new Parser(lexer); 
-				Start ast = parser.parse();
-				
-				stderr = new PrintWriter(System.out, true);
-				
-				TokenMapper tm = new TokenMapper();
-			    ast.apply(tm);
-			    tokenMap = tm.getMap();*/
-			    
-				// Get file name
-				path = args[0];
-				if (path.endsWith(".go")) {
-					path = path.substring(0, path.length() - 3);
-				}
-				
-				TokenPrintHtml(lexer);
-				
-				//PrettyPrint(ast);
-				
-				/*if (TypeCheck(ast)) {
-					// Generate C Code
-					PrintWriter fileCCode = new PrintWriter(path+".c");
-					CodeGenerator cgen = new CodeGenerator(fileCCode, typechecker.types, typechecker.symbols) ; 
-					ast.apply(cgen);
-					fileCCode.close();
-					
-					System.out.println("VALID, C code generated to " + path + ".c");
-				} */
-				
-				/*
-				// Display Typed AST in console
-				ASTPrinter typed = new ASTPrinter(typechecker.types);
-				ast.apply(typed);
-				
-				// Display AST in a JTree
-				ASTDisplay display = new ASTDisplay();
-				ast.apply(display);
-				
-				// Print Symbol Table
-				for (String id : typechecker.symbols.keySet())
-					System.out.println("<" + id + "> is " + String.valueOf(typechecker.symbols.get(id)));
-				*/
-			} catch (golite.lexer.LexerException e) {
-				System.out.println("INVALID (lexer error) at " + e.getMessage());
-			/*} catch (golite.parser.ParserException e) { 
-				System.out.println("INVALID (scanner error) at " + e.getMessage());*/
-			} catch (FileNotFoundException e) {
-				System.out.println("Error: File \"" + args[0] + "\" not found.");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.out.println(e);
-			}
-		} else { 
-			System.err.println("Usage: golite inputFile"); 
+		Lexer lexer;
+		Parser parser;
+		Node ast;
+
+		if (args.length != 1) {
+			System.err.println("Usage: golite inputFile");
 			System.exit(1);
+		}
+
+		// Get file name
+		path = args[0];
+		if (path.endsWith(".go")) {
+			path = path.substring(0, path.length() - 3);
+		}
+
+		try {
+			/* Print HTML representation of tokens */
+			TokenPrintHtml(new ConservingGoLexer(new PushbackReader(new FileReader(args[0]), 1024)));
+
+			/* Build AST */
+			lexer = new GoLexer(new PushbackReader(new FileReader(args[0]), 1024));
+			parser = new Parser(lexer);
+			ast = parser.parse();
+
+			// Display AST in a JTree
+			ASTDisplay display = new ASTDisplay();
+			ast.apply(display);
+
+			/*TokenMapper tm = new TokenMapper();
+			ast.apply(tm);
+			tokenMap = tm.getMap();*/
+
+			//PrettyPrint(ast);
+
+			/*if (TypeCheck(ast)) {
+				// Generate C Code
+				PrintWriter fileCCode = new PrintWriter(path+".c");
+				CodeGenerator cgen = new CodeGenerator(fileCCode, typechecker.types, typechecker.symbols) ;
+				ast.apply(cgen);
+				fileCCode.close();
+
+				System.out.println("VALID, C code generated to " + path + ".c");
+			} */
+
+			/*
+			// Display Typed AST in console
+			ASTPrinter typed = new ASTPrinter(typechecker.types);
+			ast.apply(typed);
+
+			// Print Symbol Table
+			for (String id : typechecker.symbols.keySet())
+				System.out.println("<" + id + "> is " + String.valueOf(typechecker.symbols.get(id)));
+			*/
+		} catch (golite.lexer.LexerException e) {
+			System.out.println("INVALID (lexer error) at " + e.getMessage());
+		} catch (golite.parser.ParserException e) {
+			System.out.println("INVALID (scanner error) at " + e.getMessage());
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: File \"" + args[0] + "\" not found.");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
 		}
 	}
 	/*
