07374b6511661c99c18e49dd868e057d777c7d6e
William Bain <bain.william.a@gmail.com>
2015-02-16 20:01:16 -0500

    GoLite-ify invalid syntax programs

--- programs/invalid/parser_invalid3.go
+++ programs/invalid/parser_invalid3.go
@@ -1,10 +1,6 @@
 package parser_invalid3
 
 func main(){
- 
-//illegal backslash sequence
-//error:unknown escape sequence: '
-println("hello\'there")
 
 a:= 5
 b:= 10
@@ -13,14 +9,4 @@ b:= 10
 s,t := compare(a,b)
 println(s,t)
 
-}
-
-func compare(y int,z int)(int,int){
-if x := 3; x > y {
-	return x,y;
-} else if x > z {
-	return x,z
-} else {
-	return y,z
-}
 }
