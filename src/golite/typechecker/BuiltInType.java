5459a4d645f2f019c5fc05ae42e893e3ec21411d
William Bain <bain.william.a@gmail.com>
2015-02-27 18:58:01 -0500

    TypeChecker: Streamline builtin type manipulation

--- src/golite/typechecker/BuiltInType.java
+++ src/golite/typechecker/BuiltInType.java
@@ -1,13 +1,7 @@
 package golite.typechecker;
 
-import java.util.Set;
-import java.util.HashSet;
-
 public class BuiltInType extends Type {
-	
-	static public HashSet<BuiltInType> builtIns = new HashSet<BuiltInType>();
-	static public HashSet<BuiltInType> typeCastTypes = new HashSet<BuiltInType>();
-	
+
 	public BuiltInType(String identifier) {
 		super(identifier);
 	}
