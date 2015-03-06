2ed38a7d8231c19aa67e684e4a71c9e8f2c6c787
sophiesuperduper <sophiezhang0919@gmail.com>
2015-03-06 10:24:58 -0500

    Update typecheck1.go
    function declaration

--- programs/valid/semantic/typecheck1.go
+++ programs/valid/semantic/typecheck1.go
@@ -3,6 +3,62 @@ package main
 
 func main() {
 
+	println("3*3/* Package declaration, golite 2.2 */
+package main
+
+func main() {
+
+	println("3*3 is:",f(3))
+	/*Variables Declarations 1.1 */
+	var num1 int
+	num1 = 12
+	var num2 int = num1 * 1.0 + 2.0
+	
+	/*Block*/
+	{var num3 = num1 * num1
+	println("In the block. num3 should be 144:", num3)
+	}
+	
+	var num3 = num1 * 3.0
+	println( "Three numbers should be 12 14 36 and they are:",num1,num2,num3 )
+	
+	/*Types Declarations 1.2 */
+	type a int 
+	var x a = 2
+	println( "x is of type a and it should be 2:",x )
+	
+	/*redeclare type in inner scope*/	
+	{type a bool
+	var x a = false 
+	println(x) 
+	}
+	
+	var res int = regularPower(2,3)
+	println("2^3 is:",res)
+	
+	
+}
+
+	
+/*Functions Declarations 1.3 */
+func f(f int) int {
+	f *=f
+	return f
+}
+func regularPower(a int, b int) int {
+	var res int
+	res = 1
+	for b>0 {
+		res *= a
+		b --
+   }
+   return res 
+}
+
+/* this example in doc doesn't work in go playground tho */
+//func f(f int) {
+//	var f float64
+//}is:"f(3))
 	/*Variables Declarations 1.1 */
 	var num1 int
 	num1 = 12
@@ -44,3 +100,8 @@ func regularPower(a int, b int) int {
    }
    return res 
 }
+
+/* this example in doc doesn't work in go playground tho */
+//func f(f int) {
+//	var f float64
+//}
