f775b22caa25b74de7d773d41e867b54d8099938
sophiesuperduper <sophiezhang0919@gmail.com>
2015-03-06 10:57:22 -0500

    Update typecheck1.go

--- programs/valid/semantic/typecheck1.go
+++ programs/valid/semantic/typecheck1.go
@@ -3,25 +3,32 @@ package main
 
 func main() {
 
-	println("3*3/* Package declaration, golite 2.2 */
-package main
-
-func main() {
-
-	println("3*3 is:",f(3))
+	/*print and println 2.10, runeType*/
+	print('a')
+	print(" ",'b')
+	/*Function Call 3.5*/
+	println("3*(-3) is:",f(3))
+	
 	/*Variables Declarations 1.1 */
 	var num1 int
 	num1 = 12
 	var num2 int = num1 * 1.0 + 2.0
 	
-	/*Block*/
+	/*Block 2.9*/
 	{var num3 = num1 * num1
 	println("In the block. num3 should be 144:", num3)
 	}
 	
 	var num3 = num1 * 3.0
-	println( "Three numbers should be 12 14 36 and they are:",num1,num2,num3 )
+	println( "Three numbers should be 12 14 36:",num1,num2,num3 )
 	
+	/*Short Declaration 2.5*/
+	/*Assignment 2.7*/
+	num4,num5 := num1+1, num2*2.0
+	var num6,num7 int
+	num6,num7 = num4-num1, num2/num5
+	println( "Three numbers should be 13 28 1 0:",num4,num5,num6,num7 )
+
 	/*Types Declarations 1.2 */
 	type a int 
 	var x a = 2
@@ -36,19 +43,34 @@ func main() {
 	var res int = regularPower(2,3)
 	println("2^3 is:",res)
 	
+	/*For Loop 2.11*/
+	for y:=2;y>0;y-- {
+		x*=x
+	}
+	println("should be 16 :",x)
+	
+	/*If statement 2.12, Type cast*/
+	if z:="error";x>a(10){
+		println(x , "is bigger", 'then', "10")
+	} else {
+		println(z + "should be bigger than 10")
+	}
 	
 }
 
 	
-/*Functions Declarations 1.3 */
+/*Functions Declarations 1.3, Unary expr 3.3 */
 func f(f int) int {
-	f *=f
+	f *=(-f)
 	return f
 }
+
 func regularPower(a int, b int) int {
 	var res int
 	res = 1
+	/*For loop 2.11*/
 	for b>0 {
+		/*Op-assignment 2.8*/
 		res *= a
 		b --
    }
@@ -59,49 +81,3 @@ func regularPower(a int, b int) int {
 //func f(f int) {
 //	var f float64
 //}is:"f(3))
-	/*Variables Declarations 1.1 */
-	var num1 int
-	num1 = 12
-	var num2 int = num1 * 1.0 + 2.0
-	
-	/*Block*/
-	{var num3 = num1 * num1
-	println("In the block. num3 should be 144:", num3)
-	}
-	
-	var num3 = num1 * 3.0
-	println( "Three numbers should be 12 14 36 and they are:",num1,num2,num3 )
-	
-	/*Types Declarations 1.2 */
-	type a int 
-	var x a = 2
-	println( "x is of type a and it should be 2:",x )
-	
-	/*redeclare type in inner scope*/	
-	{type a bool
-	var x a = false 
-	println(x) 
-	}
-	
-	var res int = regularPower(2,3)
-	println("2^3 is:",res)
-	
-	
-}
-
-	
-/*Functions Declarations 1.3 */
-func regularPower(a int, b int) int {
-	var res int
-	res = 1
-	for b>0 {
-		res *= a
-		b --
-   }
-   return res 
-}
-
-/* this example in doc doesn't work in go playground tho */
-//func f(f int) {
-//	var f float64
-//}
