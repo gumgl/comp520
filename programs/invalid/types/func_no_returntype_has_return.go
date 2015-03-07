9656b460e83c4d5ae7fb3fcb4d3044710d50c1b6
William Bain <bain.william.a@gmail.com>
2015-03-07 01:28:32 -0500

    Complete milestone doc

--- programs/invalid/types/func_no_returntype_has_return.go
+++ programs/invalid/types/func_no_returntype_has_return.go
@@ -1,13 +1,12 @@
 package main
 
-func main(){
-
-	adder(2,3)
-}
-
 //error: adder shouldnt have return argument
 func adder (a int, b int) {
-	var c = a+b
-	print(c)
-	return c
+    var c = a+b
+    print(c)
+    return c
+}
+
+func main(){
+	adder(2,3)
 }
