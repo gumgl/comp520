dcd08bb65e80e78d775f5f1100496239dd39fa43
William Bain <bain.william.a@gmail.com>
2015-03-22 02:15:54 -0400

    Test programs: Fix random broken test

--- programs/invalid/teams/types/group3/bad_cast2.go
+++ programs/invalid/teams/types/group3/bad_cast2.go
@@ -1,6 +1,6 @@
 package main
 
-struct person {
+type person struct {
 	name string
 	age int
 }
@@ -8,5 +8,5 @@ struct person {
 func main() {
 	// This doesn't type check because you cannot typecast to a struct
 	bob := "Bob is 36 years old"
-	println((person)bob)
+	println(person(bob))
 }
