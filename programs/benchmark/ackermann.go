890b68abe4c11e04544c63be36b38bd7d47cd34a
William Bain <bain.william.a@gmail.com>
2015-03-29 03:24:31 -0400

    Benchmark: modify Ackermann to use a stack instead of recursing

--- programs/benchmark/ackermann.go
+++ programs/benchmark/ackermann.go
@@ -2,17 +2,52 @@ package main
 
 // Using Go, this runs in about 20 seconds on my laptop
 
+type stack struct{
+	store []int;
+	index, allocated int;
+}
+
+func push(s stack, value int) stack {
+	s.index++
+
+	if s.allocated >= s.index {
+		s.store[s.index-1] = value
+	} else {
+		s.allocated++
+		var store = s.store
+		s.store = append(store, value)
+	}
+
+	return s
+}
+
 func ackermann(m, n int) int {
-	if m > 0 && n == 0 {
-		m--
-		n = 1
+    var s stack
+
+    s = push(s, m)
+    s = push(s, n)
+
+    for s.index > 1 {
+		var m, n = s.store[s.index-2], s.store[s.index-1]
+		s.index -= 2
+
+		if m > 0 && n == 0 {
+			m--
+			n = 1
+		}
+
+		if m == 0 {
+			s = push(s, n + 1)
+			continue
+		}
+
+		// ackermann(m-1, ackermann(m, n-1))
+		s = push(s, m-1)
+		s = push(s, m)
+		s = push(s, n-1)
 	}
 
-	if m == 0 {
-		return n + 1
-	}
-
-	return ackermann(m-1, ackermann(m, n-1))
+	return s.store[0]
 }
 
 func main() {
