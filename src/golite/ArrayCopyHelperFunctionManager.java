a3b686658ef173a4769c899851f6e038c689bc06
William Bain <bain.william.a@gmail.com>
2015-04-10 21:57:47 -0400

    JSGen: Add abstract class for array helper function managers

--- src/golite/ArrayCopyHelperFunctionManager.java
+++ src/golite/ArrayCopyHelperFunctionManager.java
@@ -5,37 +5,14 @@ import golite.node.AVariableExp;
 import golite.node.PExp;
 import golite.node.TId;
 import golite.typechecker.ArrayType;
-import golite.typechecker.Type;
 
-public class ArrayCopyHelperFunctionManager extends JSHelperFunctionManager<ArrayType> {
+public class ArrayCopyHelperFunctionManager extends ArrayHelperFunctionManager {
 
 	private static final PExp arrayAccessExp =
 			new AArrayAccessExp(new AVariableExp(new TId("a")), new AVariableExp(new TId("i")));
 
 	public ArrayCopyHelperFunctionManager(String prefix, JSGenerator out) {
 		super(prefix, 2, out);
-	}
-
-	protected boolean areEquivalentContainedTypes(Type t1, Type t2) {
-		t1 = t1.getUnderlying();
-		t2 = t2.getUnderlying();
-
-		if (t1 instanceof ArrayType) {
-			if (t2 instanceof ArrayType) {
-				return areEquivalent((ArrayType) t1, (ArrayType) t2);
-			}
-
-			return false;
-		}
-
-		return t1.isIdentical(t2);
-	}
-
-	protected boolean areEquivalent(ArrayType t1, ArrayType t2) {
-		if (t1 == t2)
-			return true;
-
-		return areEquivalentContainedTypes(t1.getType(), t2.getType());
 	}
 
 	@Override
