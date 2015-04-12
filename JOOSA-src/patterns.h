3430cd27c2268d36c69bbeccc64b9c5e371c8737
William Bain <bain.william.a@gmail.com>
2015-04-11 23:43:39 -0400

    Whitespace fixes

--- JOOSA-src/patterns.h
+++ JOOSA-src/patterns.h
@@ -21,8 +21,8 @@
 
 int simplify_multiplication_right(CODE **c)
 { int x,k;
-  if (is_iload(*c,&x) && 
-      is_ldc_int(next(*c),&k) && 
+  if (is_iload(*c,&x) &&
+      is_ldc_int(next(*c),&k) &&
       is_imul(next(next(*c)))) {
      if (k==0) return replace(c,3,makeCODEldc_int(0,NULL));
      else if (k==1) return replace(c,3,makeCODEiload(x,NULL));
@@ -56,7 +56,7 @@ int simplify_astore(CODE **c)
  * istore x
  * --------->
  * iinc x k
- */ 
+ */
 int positive_increment(CODE **c)
 { int x,y,k;
   if (is_iload(*c,&x) &&
@@ -81,7 +81,7 @@ int positive_increment(CODE **c)
  * L1:    (reference count reduced by 1)
  * goto L2
  * ...
- * L2:    (reference count increased by 1)  
+ * L2:    (reference count increased by 1)
  */
 int simplify_goto_goto(CODE **c)
 { int l1,l2;
